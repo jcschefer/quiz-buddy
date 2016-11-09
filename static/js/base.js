7f7f8b277c59f4bd4e3dffe392dd71b9b552cc6e
Jack Schefer <jack.schefer9@gmail.com>
2016-11-09 10:35:45 -0500

    popup

---
+++ static/js/base.js
@@ -0,0 +1,21 @@
+// Jack Schefer, began 10/21/16 
+//
+//
+$( document ).ready( function() {
+   //
+   console.log('base.js loaded') ;
+   //
+   $( "#popup" ).hide() ;
+   //
+   $( "#play-link" ).click( function() {
+      console.log("clicked") ;
+      $( "#popup" ).show() ;
+   });
+   //
+}) ;
+//
+//
+//
+//
+//
+// End of file.
