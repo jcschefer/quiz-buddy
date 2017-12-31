6e0880e36f77f37350c2bd4db11547701d38e52b
Jack Schefer <jack.schefer9@gmail.com>
2017-12-30 19:38:22 -0500

    django: setup the database, make models, make migrations

--- web/gameplay/models.py
+++ web/gameplay/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tournament(models.Model):
+    name = models.CharField(max_length = 200)
+    year = models.IntegerField()
+
+class Packet(models.Model):
+    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 200)
+    index = models.IntegerField(help_text='Order that the packet comes in the tournament')
+
+class Question(models.Model):
+    packet_id = models.ForeignKey(Packet, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1500)
+    answer = models.CharField(max_length=250)
