2e54c2c33cde63d6a3bbd81ec1b72237a58e3a73
Jack Schefer <jack.schefer9@gmail.com>
2018-08-12 16:12:04 -0400

    added ui page for download links, updated logo for new name

--- web/gameplay/urls.py
+++ web/gameplay/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about.html', views.about, name='about'),
+    path('guides.html', views.guides, name='guides'),
     path('play.html', views.play, name='play'),
 ]
--- web/gameplay/views.py
+++ web/gameplay/views.py
@@ -3,11 +3,19 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse(get_template('gameplay/index.html').render())
 
+
 def about(request):
     return HttpResponse(get_template('gameplay/about.html').render())
+
+
+def guides(request):
+    return HttpResponse(get_template('gameplay/guides.html').render())
+
 
 def play(request):
     return HttpResponse(get_template('gameplay/play.html').render())
